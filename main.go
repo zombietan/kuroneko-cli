c3611d6bc938c6cbc3ef8bc7120f26e8e5d248f2
fatcat7 <fatcat7@aol.jp>
2017-04-18 18:02:18 +0900

    Additional serial number acquisition function added

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/urfave/cli"
@@ -12,6 +13,27 @@ func main() {
 	app.Name = "kuroneko"
 	app.Usage = "Display delivery status"
 	app.Version = "0.0.0"
-	app.Action = cmd.Kuroneko
+	app.Flags = []cli.Flag{
+		cli.UintFlag{
+			Name:  "serial, s",
+			Usage: "連番は10件まで",
+		},
+	}
+	app.Action = func(c *cli.Context) error {
+		set := c.GlobalIsSet("serial")
+		if set {
+			items := c.Uint("serial")
+			if 1 <= items && items <= 10 {
+				cmd.TrackSerialNumbers(c)
+			} else {
+				fmt.Println("連番で取得できるのは10件までです")
+			}
+		} else {
+			cmd.TrackNumber(c)
+		}
+
+		return nil
+	}
+
 	app.Run(os.Args)
 }
